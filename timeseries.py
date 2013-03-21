5ee14d216c61a10346d8468cc3e434bce551b4d4
TJ Giuli <tgiuli@ford.com>
2013-03-21 00:09:35 -0700

    Init commit of a script that will directly graph vehicle data

---
+++ timeseries.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+#Go through an OpenXC trace file and plot a time series graph using 
+#matplotlib
+
+import json
+import sys
+import argparse
+from pylab import *
+
+
+#Set up the command line argument parser
+parser = argparse.ArgumentParser()
+
+parser.add_argument("input_file",
+                    help = "name of the input file")
+parser.add_argument("-y",
+                    help = "the key to use for the function being plotted")
+
+args = parser.parse_args()
+
+input_file_name = args.input_file
+
+y_key = str(args.y)
+
+#initialize the x axis and function to be plotted
+x = []
+y = []
+
+with open(input_file_name, 'r') as input_file:
+    for line in input_file:
+        if not line.rstrip() : continue
+        obj = json.loads(line)
+            
+        #if the parsed JSON object has the key we're looking for,
+        #add the key's value to the y graph and the timestamp
+        #to the x list
+        if obj['name'] == y_key:
+            y.append(obj['value'])
+            x.append(obj['timestamp'])
+            
+    autoscale(True, 'both')
+    plot(x, y, label = y_key)
+    legend(loc='upper left')
+    show()
