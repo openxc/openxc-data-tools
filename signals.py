bd7d157ae7d2141bbf336a192bcc5120c18f2e5d
Zac Nelson <znelson1@ford.com>
2013-03-27 11:45:32 -0400

    add signals.py which prints out the list of signal names contained in a json trace file.

---
+++ signals.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+#Go through an OpenXC trace file and print out a list
+#of all present signals.
+
+import json
+import sys
+import argparse
+
+def main():
+    #Set up the command line argument parser
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input_file",
+                        help = "name of the input file")
+
+    args = parser.parse_args()
+
+    input_file_name = args.input_file
+    
+    signal_list = list()
+	
+    with open(input_file_name, 'r') as input_file:
+        for line in input_file:
+            if not line.rstrip() : continue
+            obj = json.loads(line)
+			
+            if obj['name'] not in signal_list:
+                signal_list.append(obj['name'])
+
+    signal_list.sort()
+    print "\n".join(signal_list)
+
+if __name__ == "__main__":
+    main()
